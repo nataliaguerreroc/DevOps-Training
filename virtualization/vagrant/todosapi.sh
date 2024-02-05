1cdc7fbda43b1fdf9b1335003626faf81bd1ae71
Natalia Guerrero Caicedo <natalia.guerrero28@gmail.com>
2024-02-05 17:16:21 -0500

    todosapi.sh changes

--- virtualization/vagrant/todosapi.sh
+++ virtualization/vagrant/todosapi.sh
@@ -17,33 +17,23 @@ if ! command -v curl &>/dev/null; then
 	sudo apt install -y curl
 fi
 
-if ! command -v nvm &> /dev/null; then
-    echo "nvm doesnt exist"
-
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-    export NVM_DIR="$HOME/.nvm"
-	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-
-else
-    echo "nvm exist"
-fi
-
-if [ "$(node -v)" == "v8.17.0" ]; then
-    echo "Node.js v. 8.17.0 exist"
-else
-    echo "Installing Node.js v 8.17.0."
-    nvm install 8.17.0
-fi
-
 cd microservice-app-example/todos-api
+sudo rm -rf node_modules package-lock.json
+
+if ! command -v npm &>/dev/null; then
+	echo "npm doesnt exist"
+    sudo apt-get update
+	sudo apt install -y npm
+else
+    echo "npm exists"
+fi
+
+npm install
 
 if pgrep -f "npm start" &>/dev/null; then
     echo "todos-api is already on"
 else
 	echo "Starting todos-api"
 	echo "Starting nohup"
-		ZIPKIN_URL=http://10.0.0.8:9411/api/v2/spans JWT_SECRET=PRFT TODO_API_PORT=8082 REDIS_HOST=10.0.0.3 REDIS_PORT=6377 REDIS_CHANNEL=log_channel nohup npm start > /home/vagrant/microservice-app-example/todos-api/nohup.out &
+	ZIPKIN_URL=http://10.0.0.8:9411/api/v2/spans JWT_SECRET=PRFT TODO_API_PORT=8082 REDIS_HOST=10.0.0.3 REDIS_PORT=6377 REDIS_CHANNEL=log_channel nohup npm start > /home/vagrant/microservice-app-example/todos-api/nohup.out &
 fi
-
